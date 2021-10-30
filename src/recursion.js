23b62fe833b44f76ca73391947161b96b6d1f6c9
Nellie Nelson <NellieNelson@protonmail.com>
2021-10-30 14:34:22 -0700

    First four exercises completed.

--- src/recursion.js
+++ src/recursion.js
@@ -7,26 +7,51 @@
 // Example: 5! = 5 x 4 x 3 x 2 x 1 = 120
 // factorial(5); // 120
 var factorial = function(n) {
+  if (n < 0) {
+    return null;
+  }
+  return (n === 1 || n === 0) ? 1 : n * factorial(n - 1);
 };
 
 // 2. Compute the sum of an array of integers.
 // sum([1,2,3,4,5,6]); // 21
 var sum = function(array) {
+  return array.length === 0 ? 0 : array[0] + sum(array.slice(1));
 };
 
 // 3. Sum all numbers in an array containing nested arrays.
 // arraySum([1,[2,3],[[4]],5]); // 15
+
 var arraySum = function(array) {
+  if (array.length === 0) {
+    return 0;
+  }
+  if (Array.isArray(array[0])) {
+    return arraySum(array[0]) + arraySum(array.slice(1));
+  }
+  return array[0] + arraySum(array.slice(1));
 };
 
 // 4. Check if a number is even.
 var isEven = function(n) {
-};
-
+  n = Math.abs(n);
+  if (n === 0 || (n === 1)) {
+    return !n;
+  }
+  return isEven(n - 2);
+}
 // 5. Sum all integers below a given integer.
 // sumBelow(10); // 45
 // sumBelow(7); // 21
 var sumBelow = function(n) {
+  // if (n = 0) {
+  //   return 0;
+  // }
+
+  // if (n < 0) {
+  //   return -(sumBelow(-n));
+  // }
+  // return n - 1 + sumBelow(n - 1);
 };
 
 // 6. Get the integers within a range (x, y).
@@ -40,6 +65,22 @@ var range = function(x, y) {
 // exponent(4,3); // 64
 // https://www.khanacademy.org/computing/computer-science/algorithms/recursive-algorithms/a/computing-powers-of-a-number
 var exponent = function(base, exp) {
+  // console.log(base, exp)
+  // if (exp = 0) {
+  //   return 1;
+  // }
+  // if (exp = 1) {
+  //   return base;
+  // }
+  // if (exp = -1) {
+  //   return 1 / base;
+  // }
+  // if (exp > 0) {
+  // return base * exponent(base, exp - 1);
+  // }
+  // if (exp < 0) {
+  //   return (1 / base) * exponent(base, exp + 1);
+  // }
 };
 
 // 8. Determine if a number is a power of two.
@@ -47,13 +88,19 @@ var exponent = function(base, exp) {
 // powerOfTwo(16); // true
 // powerOfTwo(10); // false
 var powerOfTwo = function(n) {
+  // console.log(n)
+  // if (n = 2) {
+  //   return true;
+  // }
+  // return powerOfTwo(Math.sqrt(n));
+
 };
 
 // 9. Write a function that reverses a string.
 var reverse = function(string) {
 };
 
-// 10. Write a function that determines if a string is a palindrome.
+// **10. Write a function that determines if a string is a palindrome.
 var palindrome = function(string) {
 };
 
@@ -131,14 +178,14 @@ var rMap = function(array, callback) {
 var countKeysInObj = function(obj, key) {
 };
 
-// 23. Write a function that counts the number of times a value occurs in an object.
+// **23. Write a function that counts the number of times a value occurs in an object.
 // var obj = {'e':{'x':'y'},'t':{'r':{'e':'r'},'p':{'y':'r'}},'y':'e'};
 // countValuesInObj(obj, 'r') // 2
 // countValuesInObj(obj, 'e') // 1
 var countValuesInObj = function(obj, value) {
 };
 
-// 24. Find all keys in an object (and nested objects) by a provided name and rename
+// **24. Find all keys in an object (and nested objects) by a provided name and rename
 // them to a provided new name while preserving the value stored at that key.
 var replaceKeysInObj = function(obj, oldKey, newKey) {
 };
