436e5187c125951994ef0b2a306fc66e2a105731
Nellie Nelson <NellieNelson@protonmail.com>
2021-10-30 17:49:48 -0700

    Completed through problem 6

--- src/recursion.js
+++ src/recursion.js
@@ -44,19 +44,25 @@ var isEven = function(n) {
 // sumBelow(10); // 45
 // sumBelow(7); // 21
 var sumBelow = function(n) {
-  // if (n = 0) {
-  //   return 0;
-  // }
-
-  // if (n < 0) {
-  //   return -(sumBelow(-n));
-  // }
-  // return n - 1 + sumBelow(n - 1);
+  if (n === 0 || Math.abs(n) === 1) {
+    return 0;
+  }
+  if (n < 0) {
+    return -(sumBelow(-n));
+  }
+  return n - 1 + sumBelow(n - 1);
 };
 
 // 6. Get the integers within a range (x, y).
 // range(2,9); // [3,4,5,6,7,8]
 var range = function(x, y) {
+  if (Math.abs(x - y) <= 1){
+    return [];
+  }
+  if (x > y) {
+    return [x - 1].concat(range(x - 1, y));
+  }
+  return [x + 1].concat(range(x + 1, y));
 };
 
 // 7. Compute the exponent of a number.
